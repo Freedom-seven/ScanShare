1d3cba3377224125d39565492d080d9f1dc6ffd9
FREEDOM <freewhynane62@gmail.com>
2021-11-30 11:22:25 +0000

    add getstarted screen

---
+++ src/Screens/GetStartedScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const GetStartedScreen = () => {
+    return (
+        <View>
+            <Text>Get started</Text>
+        </View>
+    )
+}
+
+export default GetStartedScreen
+
+const styles = StyleSheet.create({})
